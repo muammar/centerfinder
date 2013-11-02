06b18997a94d1a5de402037926ce20e6c0524f55
Muammar El Khatib <muammarelkhatib@gmail.com>
2013-11-03 00:40:53 +0100

    Lines were splitted, and lists files are being created

--- centers.py
+++ centers.py
@@ -15,6 +15,21 @@ with open('input') as infile, open('coord','w') as outfile:
             outfile.write(line)
 
 """
+Lists are created to be treated later.
+"""
+import simplejson
+with open('coord') as inf, open('coordlst','w') as outfile:
+    for line in inf:
+        parts = line.split() # split line into parts
+        if len(parts) > 1:   # if at least 2 parts/columns
+            #print parts[3], parts[4], parts[5]
+            coordvec=[parts[3],parts[4],parts[5]]
+            #print coordvec
+            simplejson.dump(coordvec, outfile)
+            outfile.close
+
+
+"""
 The same process is done now to take out the centers of charges from the MOLPRO
 output file.
 """
@@ -27,4 +42,14 @@ with open('input') as infile, open('coc','w') as centersoc:
                 copy = False
             elif copy:
                 centersoc.write(line)
+
+with open('coc') as inf, open('coclst', 'w') as outfile:
+    for line in inf:
+        parts = line.split() # split line into parts
+        if len(parts) > 1:   # if at least 2 parts/columns
+            #print parts[2], parts[3], parts[4]
+            cocvec=[ parts[2], parts[3], parts[4]]
+            print cocvec
+            simplejson.dump(cocvec, outfile)
+            outfile.close
 
