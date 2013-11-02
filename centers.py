11b2ca3515426f053c29685dc38664d08ee0b326
Muammar El Khatib <muammarelkhatib@gmail.com>
2013-11-02 17:33:37 +0100

    Adding centers.py

---
+++ centers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+with open('input') as infile, open('output', 'w') as outfile:
+    copy = False
+    for line in infile:
+        if line.strip() == "NR  ATOM    CHARGE       X              Y              Z":
+            copy = True
+        elif line.strip() == "Bond lengths in Bohr (Angstrom)":
+            copy = False
+        elif copy:
+            outfile.write(line)
