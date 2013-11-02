72ab1ec5539fea3a0eb332d5a4963ba21dfc281d
Muammar El Khatib <muammarelkhatib@gmail.com>
2013-11-02 18:15:00 +0100

    examples directory created. Centers of charge are readed from MOLPRO output file

--- centers.py
+++ centers.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
-with open('input') as infile, open('output', 'w') as outfile:
+
+"""
+In this part of the code, we take the coordinates of the molecule from the
+MOLPRO output file and then we dump its content in outfile.
+"""
+with open('input') as infile, open('coord','w') as outfile:
     copy = False
     for line in infile:
         if line.strip() == "NR  ATOM    CHARGE       X              Y              Z":
@@ -8,3 +13,18 @@ with open('input') as infile, open('output', 'w') as outfile:
             copy = False
         elif copy:
             outfile.write(line)
+
+"""
+The same process is done now to take out the centers of charges from the MOLPRO
+output file.
+"""
+with open('input') as infile, open('coc','w') as centersoc:
+    copy = False
+    for line in infile:
+            if line.strip() == "Sym.   Orb.            X            Y            Z":
+                copy = True
+            elif line.strip() == "Localized orbitals saved to record      2103.2  (orbital set 1)":
+                copy = False
+            elif copy:
+                centersoc.write(line)
+
