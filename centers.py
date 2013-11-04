c989dc423fd86e1b8700b7e1f1659d70a7acef05
Muammar El Khatib <muammarelkhatib@gmail.com>
2013-11-04 11:58:09 +0100

    Coordinates are now successfully converted to csv, and the imported as an array in numpy

--- centers.py
+++ centers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import csv
 """
 In this part of the code, we take the coordinates of the molecule from the
 MOLPRO output file and then we dump its content in outfile.
@@ -23,6 +24,22 @@ with open('coord') as fr, open('coordv', 'w') as fw:
     for line in fr:
         fw.write(','.join(line.strip().split()) + '\n')
 
+input = open('coordv', 'rb')
+output = open('coordvout', 'wb')
+writer = csv.writer(output)
+for row in csv.reader(input):
+    if row:
+        writer.writerow(row)
+input.close()
+output.close()
+
+with open('coordvout','rb') as source:
+    rdr= csv.reader( source )
+    with open('coordbarray','wb') as result:
+        wtr= csv.writer(result)
+        for r in rdr:
+            wtr.writerow( (r[3], r[4], r[5]) )
+
 """
 The same process is done now to take out the centers of charges from the MOLPRO
 output file.
@@ -44,7 +61,6 @@ with open('coc') as infile, open('cocsv', 'w') as outfile:
     outfile.write(infile.read().replace("    ", ", "))
 
 # Unneeded columns are deleted from the csv
-import csv
 input = open('cocsv', 'rb')
 output = open('cocsvout', 'wb')
 writer = csv.writer(output)
@@ -54,14 +70,15 @@ for row in csv.reader(input):
 input.close()
 output.close()
 
-with open("cocsvout","rb") as source:
+with open('cocsvout','rb') as source:
     rdr= csv.reader( source )
-    with open("cocbarray","wb") as result:
+    with open('cocbarray','wb') as result:
         wtr= csv.writer(result)
         for r in rdr:
             wtr.writerow( (r[3], r[4], r[5]) )
 
+
 # Import csv to matrix in numpy.
 from numpy import genfromtxt
 cocmatrix = genfromtxt('cocbarray', delimiter=',')
-
+coordmatrix = genfromtxt('coordbarray', delimiter=',')
